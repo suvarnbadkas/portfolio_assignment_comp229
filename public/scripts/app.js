b0196f322bd8715e439ab1354abfe7bd37ec7ecf
suvarn badkas <badkassuvarn@gmail.com>
2022-02-05 11:02:58 -0500

    initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+    function start()
+    {
+        console.log("App started...");
+    }
+
+    window.addEventListener("load",start);
+
+})();
