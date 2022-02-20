6449e0ae2223cd52e331a1a1fdf42952f08316b6
suvarn badkas <badkassuvarn@gmail.com>
2022-02-19 21:26:02 -0500

    Authenication added

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://admin:admin@dbserver.8tbxg.mongodb.net/contactlist?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
